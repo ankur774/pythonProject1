71312fda10c8249dee496afe22df1271c6be83be
ankur774 <133840910+ankur774@users.noreply.github.com>
2023-05-17 16:41:42 +0530

    Add files via upload

---
+++ examplecopy8.py
@@ -0,0 +1,23 @@
+
+
+s = "aNKUR"
+a = 10
+print(s)
+print()
+print()
+print()
+def bhajan1(first) -> None:
+    name = "mohan"
+    print(name)
+    print("Hello World"+" "+first )
+
+
+bhajan1("rajkumar")
+
+
+
+last_name = "goyal"
+print(last_name)
+print()
+print()
+print("daga")
