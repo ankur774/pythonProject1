8b3a08fe0c9829ba4dd95f7527b30f9433a31694
ankur774 <133840910+ankur774@users.noreply.github.com>
2023-05-17 16:42:42 +0530

    Update examplecopy8.py

--- examplecopy8.py
+++ examplecopy8.py
@@ -6,18 +6,3 @@ print(s)
 print()
 print()
 print()
-def bhajan1(first) -> None:
-    name = "mohan"
-    print(name)
-    print("Hello World"+" "+first )
-
-
-bhajan1("rajkumar")
-
-
-
-last_name = "goyal"
-print(last_name)
-print()
-print()
-print("daga")
